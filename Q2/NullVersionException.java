46067f58484d213595ee2276d0496e4f0512e340
KabyDaKabenz <76658191+KabyDaKabenz@users.noreply.github.com>
2021-09-20 22:31:39 -0400

    Updated Exception Class so it holds documentation.

--- Q2/NullVersionException.java
+++ Q2/NullVersionException.java
@@ -1,6 +1,14 @@
-
+/**
+ * @author Karim Ibrahim
+ * 
+ * Exception class used for validating version strings passed to the comparison method.
+ *
+ */
 public class NullVersionException extends Exception{
 
+	/**
+	 * Default constructor for NullVersionException.
+	 */
 	public NullVersionException() {
 		super("Version passed cannot be null.");
 	}
